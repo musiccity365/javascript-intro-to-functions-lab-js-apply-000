6113b5bee948b200b8ef34dde4db32b53e6ea853
Michael Sackstein <msackste@masonlive.gmu.edu>
2020-01-12 19:15:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,5 +15,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
+  var lowercase = 'hello!'
+ 
+  lowercase.toLowerCase() === lowercase // true
   
+  return string.toLowerCase()
 }
